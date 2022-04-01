f85f63fddf35e9f356c2caed1503335eb5d4dfe7
Chet Corcos <ccorcos@gmail.com>
2022-04-01 16:55:33 -0700

    Docs.

--- README.md
+++ README.md
@@ -1,12 +1,107 @@
-# TypeScript Library Boilerplate
+# Defunctionalized API
+
+This library helps you to define a composable [defunctionalized](https://blog.sigplan.org/2019/12/30/defunctionalization-everybody-does-it-nobody-talks-about-it/) fluent API for constructing a serializable query that can get evaluated in a remote process.
 
 ## Getting Started
 
 ```sh
-git clone git@github.com:ccorcos/typescript-library.git library
-cd library
-git remote remove origin
-npm run build
-npm version major
-npm publish
+npm install --save defunctionalized-api
 ```
+
+## Background
+
+I built a testing framework where I have a bunch of API methods that call into the renderer process. Things like:
+
+```ts
+getElementRect(cssSelector: string): Rect
+getElementText(cssSelector: string): string
+getRectsOfElementsThatContainText(cssSelector: string, text: string): Rect[]
+```
+
+As you can see, this API does not compose well so we end up writing more and more functions that simply combine a small set of promative functions.
+
+Thus, the goal is to be able to reduce `getRectsOfElementsThatContainText` into `getElements(cssSelector).filterTextContains(text).mapGetRects()`.
+
+(One improvement for this API is to seemlessly handle arrays so we can map and filter more generally.)
+
+To do this, we start by defining a fluent API:
+
+```ts
+type RootQuery = {
+	getElement(cssSelector: strong): ElementQuery
+	getElements(cssSelector: strong): ElementsQuery
+}
+
+type ElementQuery = {
+	getRect(): Rect
+	getText(): string
+}
+
+type ElementsQuery = {
+	filterTextContains(text: string): ElementsQuery
+	mapGetRect(): Rect[]
+	atIndex(index: number): ElementQuery
+}
+```
+
+At this point this point, you can construct query plans from your testing process:
+
+```ts
+const q = queryBuilder<RootQuery>()
+const plan = q.getElements("button").filterTextContains("submit").atIndex(0).mapGetRects()
+```
+
+Now we just need to build some evaluators in the renderer process to evaluate this plan:
+
+```ts
+
+class RootQueryEvaluator implements RootQuery {
+	getElement(cssSelector: string) {
+		const element = document.querySelector(cssSelector)
+		if (!element) throw new Error("Could not find element: " + cssSelector)
+		return new ElementQueryEvaluator(element)
+	}
+	getElements(cssSelector: string) {
+		const elements = document.querySelectorAll(cssSelector)
+		if (elements.length === 0) throw new Error("Could not find elements: " + cssSelector)
+		return new ElementsQueryEvaluator(elements)
+	}
+}
+
+class ElementQueryEvaluator implements ElementQuery {
+	constructor(public element: HTMLElement) {}
+	getRect() {
+		return this.element.getClientBoundingRect()
+	}
+	getText() {
+		return this.element.innerText
+	}
+}
+
+class ElementsQueryEvaluator implements ElementsQuery {
+	constructor(public elements: HTMLElement[]) {}
+	filterTextContains(text: string) {
+		return new ElementsQueryEvaluator(
+			this.elements.filter(element => element.innerText.includes(text))
+		)
+	}
+	mapGetRect() {
+		return this.elements.map(element => element.getClientBoundingRect())
+	}
+	atIndex(index: number) {
+		const element = this.elements[index]
+		if (!element) throw new Error("Index out of range: " + index)
+		return new ElementQuery(element)
+
+	}
+}
+```
+
+Now, given a set of steps from a QueryPlan, we can evaluate the result:
+
+```ts
+const result = evaluateQuerySteps(new RootQueryEvaluator(), plan.$steps)
+```
+
+And now all we need to do is wire this up to a remote procedure call we're good to go!
+
--- test/index.ts
+++ test/index.ts
@@ -1,6 +1,132 @@
 import test from "ava"
-import { add } from "../src/index"
+import {
+	evaluateQueryPlan,
+	evaluateQuerySteps,
+	queryBuilder,
+	QueryStep,
+} from "../src"
 
-test("add", t => {
-	t.is(add(1, 2), 3)
+// ============================================================================
+// API Types
+// ============================================================================
+
+type RootQuery = {
+	getPerson(id: string): PersonQuery
+	getPeopleNamed(name: string): PeopleQuery
+}
+
+type PersonQuery = {
+	getName(): string
+	getAge(): number
+	isOlderThan(age: number): boolean
+}
+
+type PeopleQuery = {
+	mapGetName(): string[]
+	mapGetAge(): number[]
+	mapIsOlderThan(age: number): boolean[]
+	filterIsOlderThan(age: number): PeopleQuery
+	atIndex(index: number): PersonQuery
+}
+
+// ============================================================================
+// Remote Process
+// ============================================================================
+
+type Person = { id: string; name: string; age: number }
+
+const people: Person[] = [
+	{ id: "1", name: "joe", age: 10 },
+	{ id: "2", name: "joe", age: 11 },
+	{ id: "3", name: "bob", age: 12 },
+	{ id: "4", name: "jeff", age: 15 },
+]
+
+class RootQueryEvaluator implements RootQuery {
+	getPerson(id: string): PersonQuery {
+		const person = people.find((person) => person.id === id)
+		if (!person) throw new Error("Could not find person: " + id)
+		return new PersonQueryEvaluator(person)
+	}
+	getPeopleNamed(name: string): PeopleQuery {
+		const persons = people.filter((person) => person.name === name)
+		return new PeopleQueryEvaluator(persons)
+	}
+}
+
+class PersonQueryEvaluator implements PersonQuery {
+	constructor(public person: Person) {}
+	getName(): string {
+		return this.person.name
+	}
+	getAge(): number {
+		return this.person.age
+	}
+	isOlderThan(age: number): boolean {
+		return this.person.age > age
+	}
+}
+
+class PeopleQueryEvaluator implements PeopleQuery {
+	constructor(public persons: Person[]) {}
+
+	mapGetName() {
+		return this.persons.map((person) => person.name)
+	}
+	mapGetAge() {
+		return this.persons.map((person) => person.age)
+	}
+	mapIsOlderThan(age: number) {
+		return this.persons.map((person) => person.age > age)
+	}
+	filterIsOlderThan(age: number): PeopleQuery {
+		return new PeopleQueryEvaluator(
+			this.persons.filter((person) => person.age > age)
+		)
+	}
+	atIndex(index: number): PersonQuery {
+		const person = this.persons[index]
+		if (!person) throw new Error("No person at index: " + index)
+		return new PersonQueryEvaluator(person)
+	}
+}
+
+const q = queryBuilder<RootQuery>()
+
+// Simulating an async call into the remote process
+const evaluate = async (steps: QueryStep[]) => {
+	// Main process
+	const serializedSteps = JSON.stringify(steps)
+
+	// Remote process
+	const deserializedSetps = JSON.parse(serializedSteps)
+	const result = evaluateQuerySteps(new RootQueryEvaluator(), deserializedSetps)
+	const serializedResult = JSON.stringify(result)
+
+	// Main process
+	const deserializedResult = JSON.parse(serializedResult)
+	return deserializedResult
+}
+
+type Assert<A, B extends A> = {}
+
+test("getPerson.getName", async (t) => {
+	const plan = q.getPerson("1").getName()
+	const name = await evaluateQueryPlan(evaluate, plan)
+	type X = Assert<typeof name, string>
+	t.deepEqual(name, "joe")
+})
+
+test("getPeopleNamed.mapGetAge", async (t) => {
+	const plan = q.getPeopleNamed("joe").mapGetAge()
+	const ages = await evaluateQueryPlan(evaluate, plan)
+	type X = Assert<typeof ages, number[]>
+	t.deepEqual(ages, [10, 11])
+})
+
+test("getPeopleNamed.filterIsOlderThan.atIndex.getAge", async (t) => {
+	const plan = q.getPeopleNamed("joe").filterIsOlderThan(10).atIndex(0).getAge()
+	const age = await evaluateQueryPlan(evaluate, plan)
+	type X = Assert<typeof age, number>
+	t.deepEqual(age, 11)
 })
